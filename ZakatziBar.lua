a51f3d6b54d5a5a37746e2101420c68cc7044f78
Markos <64923455+gravenuance@users.noreply.github.com>
2020-06-14 11:47:25 +0100

    Update ZakatziBar.lua

--- ZakatziBar.lua
+++ ZakatziBar.lua
@@ -389,9 +389,9 @@ local function ZB_UpdateText(bar, i)
     if (bar[i].cooldown > 60) then
         bar[i].text:SetText(string.format("%.0fm", floor(bar[i].cooldown/60)))
     elseif (bar[i].cooldown >= 10) then
-        bar[i].text:SetText(string.format(" %.0f", floor(bar[i].cooldown-0.4)))
+        bar[i].text:SetText(string.format(" %.0f", floor(bar[i].cooldown)))
     else
-        bar[i].text:SetText(string.format("  %.0f", floor(bar[i].cooldown-0.4)))
+        bar[i].text:SetText(string.format("  %.0f", floor(bar[i].cooldown)))
     end
 end
 
